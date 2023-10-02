a5549f1d3819aeac5a534ee75351a9518eca647d
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2023-10-02 18:08:01 -0400

    Completed add person, student and teacher classes milestone

---
+++ person.rb
@@ -0,0 +1,28 @@
+class Person
+  # Getter for id
+  attr_reader :id
+
+  # Getter and Setter for name and age
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  # Return true if @age is greater or equal to 18 and false if not
+  def of_age?
+    return true if @age >= 18
+
+    false
+  end
+
+  # Make :of_age method private
+  private :of_age
+
+  def can_use_services
+    of_age? || @parent_permission ? true : false
+  end
+end
